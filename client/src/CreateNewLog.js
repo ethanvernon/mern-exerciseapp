b9fe09c9dbe0a8cc83a68f4f9f655a01bc8a7100
Ethan Vernon <ethan.vernon@colorado.edu>
2019-04-02 13:26:38 +0800

    adds and incorporates CreateNewLog.js

--- client/src/CreateNewLog.js
+++ client/src/CreateNewLog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Form, Input } from 'reactstrap';
+import { Button, Form, FormGroup, Input, InputGroupAddon, InputGroupText, InputGroup, Label } from 'reactstrap';
 import './App.css';
 
 
@@ -27,34 +27,46 @@ export class CreateNewLog extends Component {
 	
 	render() {
 		return (
-			<div className='form-container'>
-				<Form inline id='url-form'>
-					<Input id='user-input' type="text" name='user' placeholder="username"
+			<div className='search-container'>
+				<FormGroup className='new-log-form'>
+					<Label for='user-input'>Username</Label>
+					<Input id='user-input' type="text" name='user' 
 						value={this.props.logNameUserInput}
 						onChange={e => this.handleChange(e, 'user')}							
 					/>
-					<Input id='pw-input' type="text" name='pw' placeholder="password"
-						value={this.props.logPasswordUserInput}
-						onChange={e => this.handleChange(e, 'password')}							
+
+
+					<Label for='pw-input'>Passkey</Label>
+					<Input id='pw-input' type="text" name='pw' 
+						value={this.props.logPasskeyUserInput}
+						onChange={e => this.handleChange(e, 'passkey')}							
 					/>
-					<Input id='description-input' type="text" name='description' placeholder="description"
+
+					<Label for='description-input'>Description</Label>
+					<Input id='description-input' type="text" name='description'
 						value={this.props.logDescriptionUserInput}
 						onChange={e => this.handleChange(e, 'description')}							
 					/>
-					<Input id='duration-input' type="text" name='duration' placeholder="duration"
+
+					<Label for='duration-input'>Duration</Label>
+					<Input id='duration-input' type="text" name='duration'
 						value={this.props.logDurationUserInput}
 						onChange={e => this.handleChange(e, 'duration')}							
 					/>
-					<Input id='date-input' type="text" name='date' placeholder="date"
+
+					<Label for='date-input'>Date</Label>
+					<Input id='date-input' type="text" name='date' placeholder="YYYY-MM-DD"
 						value={this.props.logDateUserInput}
 						onChange={e => this.handleChange(e, 'date')}							
 					/>
-					<Button id='inline-button' color='primary' value="POST LOG" 
+					<Button  
+						color='primary' 
 						onClick={this.handleClick}
+						className='log-form-submit'
 					>
-						LOG
+						submit
 					</Button>
-				</Form>
+				</FormGroup>
 			</div>	
 		);
 	}
