35e2d0cce4f7f5860005b180e084b03c33808777
Ethan Vernon <ethan.vernon@colorado.edu>
2019-04-02 20:27:52 +0800

    sorts results by date

--- server.js
+++ server.js
@@ -158,6 +158,7 @@ app.get('/api/exercise/log', (req, res) => {
 	query=query.where('userName').equals(user);
 	query=query.limit(limit);
 	query=query.select({date:1, description:1, duration:1, _id:0});
+	query=query.sort({date:1});
 
 	query.exec((err, data) => {
 		if (err) throw err;	
